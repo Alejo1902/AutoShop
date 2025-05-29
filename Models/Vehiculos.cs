a6d0d566b63176f88cb825be7fa3b20263580d0e
Alej01902 <alejo29cl@gmail.com>
2025-05-28 20:25:33 -0500

    Carpeta Models y Clase vehiculos

---
+++ Models/Vehiculos.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoShop.Models
+{
+    public class Vehiculos
+    {
+        public int IdVehiculo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public string Color { get; set; }
+        public String Placa { get; set; }
+        public int IdCliente { get; set; }
+    }
+}
