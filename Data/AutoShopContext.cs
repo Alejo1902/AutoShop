af1a5056db919a2e89e7df77bcd4e485adb192b4
Alejorico618 <d.alejandrorico2@gmail.com>
2025-05-29 15:31:10 -0500

    clase AutoShopContext

---
+++ Data/AutoShopContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AutoShopManager.Models;
+
+namespace AutoShopManager.Data
+{
+    public class AutoShopContext : DbContext
+    {
+        public AutoShopContext(DbContextOptions<AutoShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Reparacion> Reparaciones { get; set; }
+        public DbSet<Partes> Partes { get; set; }
+        public DbSet<Citas> Citas { get; set; }
+
+
+    }
+}
