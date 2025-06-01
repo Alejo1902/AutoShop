ec20f39b41e0d0f3614e39bcfa2b295d2d893af8
felipegit@outlook.com <felipegit@outlook.com>
2025-05-31 19:00:52 -0500

    Añadir funcionalidad de registro de usuario
    Se han añadido las directivas `using` necesarias y se ha implementado la clase `RegisterModel` para manejar el registro de usuarios. Se incluye un constructor para inicializar el contexto de la base de datos, una propiedad `User` vinculada al formulario, y el método `OnPostAsync` que valida el registro, verifica la existencia del nombre de usuario, hashea la contraseña y guarda el nuevo usuario en la base de datos. Tras un registro exitoso, se redirige al usuario a la página de inicio de sesión.

--- Pages/Autentificacion/Register.cshtml.cs
+++ Pages/Autentificacion/Register.cshtml.cs
@@ -1,12 +1,49 @@
+using AutoShop.Models;
+using AutoShopManager.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoShop.Pages.Autentificacion
 {
     public class RegisterModel : PageModel
     {
+        private readonly AutoShopContext _context;
+
+        public RegisterModel(AutoShopContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Users User { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            // Verifica si ya existe un usuario con el mismo correo
+            var exists = await _context.Users.AnyAsync(u => u.Name == User.Name);
+            if (exists)
+            {
+                ModelState.AddModelError(string.Empty, "Ya existe una cuenta con este Nombre.");
+                return Page();
+            }
+
+            // Hashea la contraseña antes de guardar
+            var hasher = new PasswordHasher<Users>();
+            User.Password = hasher.HashPassword(User, User.Password);
+
+            _context.Users.Add(User);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Login");
         }
     }
 }
